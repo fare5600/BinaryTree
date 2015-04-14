28f711baadc2d61339d038e743a0a2e083a3ef5d
Fare Fare <fare5600@yahoo.com>
2015-04-14 10:16:46 -0500

    updated binary tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -82,6 +82,9 @@ public class BinaryTree
 		//new parent is
 		pivot.rightTree = pivP;
 		pivP.parent = pivot;
+		
+		//update all of the depths under pivot
+		pivot.updateDepths(pivot.depth-1);
 	}
 	
 	private void rotateLeft(BinaryTree pivot)
@@ -131,6 +134,9 @@ public class BinaryTree
 		//new parent is
 		pivot.leftTree = pivP;
 		pivP.parent = pivot;
+		
+		//update all of the depths under pivot
+		pivot.updateDepths(pivot.depth-1);
 	}
 	
 	public boolean search(int value)
@@ -320,5 +326,51 @@ public class BinaryTree
 				this.rightTree.add(value);
 			}
 		}
+		
+		//am I the top level root tree?
+		if(this.parent == null)
+		{
+			//do we need to rebalance?
+			if(!this.isBalanced())
+			{
+				if(this.leftTree == null)
+				{
+					//the right tree is out of balance
+					this.rightTree.rotateRight(this.rightTree.leftTree);
+				}
+				else
+					{
+						this.leftTree.rotateRight(this.leftTree.leftTree);
+					}
+				}
+				else if(this.rightTree == null)
+				{
+					//the left tree is out of balance
+					this.leftTree.rotateLeft(this.leftTree.rightTree);
+					//missing a line
+				}
+				else
+				{
+					//we know we have a left and a right tree
+					if(this.leftTree.getMaxDepth() > this.rightTree.getMaxDepth())
+					{
+						this.leftTree.rotateLeft(this.leftTree.rightTree);
+						//missing a line
+						
+					}
+					else
+					{ 
+						this.leftTree.rotateLeft(this.leftTree.rightTree);
+					}
+					else
+				
+					{
+						this.rightTree.rotateRight(this.rightTree.leftTree);
+						//missing a line
+					}
+				}
+		
+			}
+		
 	}
 }
